88a9da64c4118b66cd70e5cca617d0c03c691026
user <hag.hispin12@gmail.com>
2026-03-10 19:42:39 +0200

    להמשיך פרוייקט

--- app/src/main/java/com/example/mathprojecteylon/pizzaproject/SignIn.java
+++ app/src/main/java/com/example/mathprojecteylon/pizzaproject/SignIn.java
@@ -1,6 +1,9 @@
 package com.example.mathprojecteylon.pizzaproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +14,46 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.mathprojecteylon.R;
 
 public class SignIn extends AppCompatActivity {
+    private EditText user;
+    private EditText pass;
+    private EditText Email;
+    private Button Save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        user.findViewById(R.id.use);
+        pass.findViewById(R.id.pass);
+        Email.findViewById(R.id.email);
+        Save.findViewById(R.id.save);
+        init();
 
+    }
+    public void init(){
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
