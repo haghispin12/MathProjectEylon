3a19d05939ab2aa62b21368f92bb51345fe9bb1e
user <hag.hispin12@gmail.com>
2026-03-15 12:23:12 +0200

    להמשיך פרוייקט

--- app/src/main/java/com/example/mathprojecteylon/pizzaproject/launcher.java
+++ app/src/main/java/com/example/mathprojecteylon/pizzaproject/launcher.java
@@ -3,17 +3,12 @@ package com.example.mathprojecteylon.pizzaproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.mathprojecteylon.R;
 
-public class LogInActivity extends AppCompatActivity {
+public class launcher extends AppCompatActivity {
    private Button signIn;
    private Button logIn;
 
@@ -21,7 +16,7 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_launcher);
         signIn.findViewById(R.id.signin);
         logIn.findViewById(R.id.login);
         init();
