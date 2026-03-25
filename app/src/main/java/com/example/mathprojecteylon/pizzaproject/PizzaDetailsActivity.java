14193781d6193284c8b01f158d55724864abb245
user <hag.hispin12@gmail.com>
2026-03-25 16:13:58 +0200

    לעשות יוזר שיעבוד לחבר XML של אייטם לעגלה לעשות אדפטר להמשיך מסך מנהלים וללמוד איך לעשות אותו להמשיך בדיטטילס

--- app/src/main/java/com/example/mathprojecteylon/pizzaproject/PizzaDetailsActivity.java
+++ app/src/main/java/com/example/mathprojecteylon/pizzaproject/PizzaDetailsActivity.java
@@ -5,16 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.mathprojecteylon.R;
 
@@ -35,14 +32,15 @@ public class PizzaDetailsActivity extends AppCompatActivity {
     private CheckBox cbMushrooms;
     private CheckBox cbOnion;
     private CheckBox cbPepper;
-    private CheckBox cbExtraCheese;
+    private CheckBox cbBlacklOlive;
     private CheckBox cbPineapple;
     private CheckBox cbTuna;
     private TextView tvTotalPrice;
     private Button btnAddToCart;
     private String pizzaName;
     private String pizzaImage;
-    private int pizzaPrice;
+    private int basePrice;
+    private int totalPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,14 +61,15 @@ public class PizzaDetailsActivity extends AppCompatActivity {
         cbMushrooms = findViewById(R.id.cbMushrooms);
         cbOnion = findViewById(R.id.cbOnion);
         cbPepper = findViewById(R.id.cbPepper);
-        cbExtraCheese = findViewById(R.id.cbExtraCheese);
+        cbBlacklOlive =findViewById(R.id.cbBlackOlive);
         cbPineapple = findViewById(R.id.cbPineapple);
         cbTuna = findViewById(R.id.cbTuna);
         tvTotalPrice = findViewById(R.id.tvTotalPrice);
         btnAddToCart = findViewById(R.id.btnAddToCart);
         pizzaName = getIntent().getStringExtra("pizzaName");
         pizzaImage = getIntent().getStringExtra("pizzaImage");
-        pizzaPrice = getIntent().getIntExtra("pizzaPrice");
+        basePrice = Integer.parseInt("pizzaPrice");
+        totalPrice=basePrice;
         init();
 
         };
@@ -85,21 +84,137 @@ public class PizzaDetailsActivity extends AppCompatActivity {
          rgSize.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
              @Override
              public void onCheckedChanged(RadioGroup group, int checkedId) {
-                 String S=pizzaPrice-10;
 
                  if (checkedId == R.id.rbSmall) {
-                     tvTotalPrice.setText("סך הכול לתשלום:"+);
+                     totalPrice=basePrice-10;
+                     tvTotalPrice.setText(totalPrice+"סך הכול לתשלום:");
                      // בחר S
                  } else if (checkedId == R.id.rbMedium) {
+                     totalPrice=basePrice;
+                     tvTotalPrice.setText(totalPrice+"סך הכול לתשלום:");
                      // בחר M
                  } else if (checkedId == R.id.rbLarge) {
+                     totalPrice=basePrice+10;
+                     tvTotalPrice.setText(totalPrice+"סך הכול לתשלום:");
                      // בחר L
                  } else if (checkedId == R.id.rbXLarge) {
+                     totalPrice=basePrice+20;
+                     tvTotalPrice.setText(totalPrice+"סך הכול לתשלום:");
                      // בחר XL
                  }
 
              }
          });
+         cbTomato.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+5;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+         cbCorn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+5;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+         cbOlives.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+5;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+         cbMushrooms.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+5;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+         cbPepper.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+5;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+         cbOnion.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+5;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+         cbPineapple.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+6;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+         cbTuna.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+8;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+         cbBlaclOlive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if (isChecked) {
+                     totalPrice=totalPrice+5;
+                     // נסמן — הוסף למחיר
+                 } else {
+                     totalPrice=totalPrice;
+                     // בוטל — הורד מהמחיר
+                 }
+             }
+         });
+
+
 
      }
     }
